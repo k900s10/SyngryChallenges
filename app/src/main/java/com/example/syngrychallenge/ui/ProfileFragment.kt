d1c3c13b750385aea924a23294a2ec063f16811a
Rahmat Alif A <rahmatalif.id@gmail.com>
2023-11-09 12:36:27 +0700

    change status bar color

--- app/src/main/java/com/example/syngrychallenge/ui/ProfileFragment.kt
+++ app/src/main/java/com/example/syngrychallenge/ui/ProfileFragment.kt
@@ -1,13 +1,11 @@
 package com.example.syngrychallenge.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.syngrychallenge.R
 import com.example.syngrychallenge.databinding.FragmentProfileBinding
 import com.example.syngrychallenge.ui.viewModel.ProfileViewModel
 import com.example.syngrychallenge.utils.Util.safeNavigate
